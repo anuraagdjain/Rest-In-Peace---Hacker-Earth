a46336af698f0add0f71d7400f10ae995fab7d25
Anuraag D Jain <thehray@gmail.com>
2016-07-31 22:00:52 +0530

    Update main.swift

--- main.swift
+++ main.swift
@@ -1,6 +1,6 @@
 //
 //  main.swift
-//  
+//  Sample
 //
 //  Created by Anuraag Jain on 31/07/16.
 //  Copyright © 2016 Anuraag Jain. All rights reserved.
@@ -13,12 +13,17 @@ while(noCases > 0 ){
     var getValue =  readLine()!
     do{
         let checkReg = try NSRegularExpression(pattern: "(21)|2([0-9]*)1", options: .CaseInsensitive)
-        if checkReg.firstMatchInString(getValue, options: .ReportCompletion, range: NSMakeRange(0, getValue.characters.count)) != nil || (Int(getValue)!%21 == 0 ) {
-            //checkReg.pattern 
+        if checkReg.firstMatchInString(getValue, options: .ReportCompletion, range: NSMakeRange(0, getValue.characters.count)) != nil{
+            checkReg.pattern
             print("The streak is broken!")
         }
         else{
+            if  (Int(getValue)!%21 == 0 ){
+                print("The streak is broken!")
+            }
+            else{
             print("The streak lives still in our heart!")
+            }
         }
     }
     catch{
@@ -26,3 +31,4 @@ while(noCases > 0 ){
     }
     noCases -= 1
 }
+
