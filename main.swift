cf6c7218bf2fa3f65119605674497ac302459364
Anuraag D Jain <thehray@gmail.com>
2016-07-31 21:38:42 +0530

    Using Regular Exp.

---
+++ main.swift
@@ -0,0 +1,28 @@
+//
+//  main.swift
+//  Sample
+//
+//  Created by Anuraag Jain on 31/07/16.
+//  Copyright © 2016 Anuraag Jain. All rights reserved.
+//
+
+import Foundation
+
+var noCases =  Int(readLine()!)!
+while(noCases > 0 ){
+    var getValue =  readLine()!
+    do{
+        let checkReg = try NSRegularExpression(pattern: "(21)|2([0-9]*)1", options: .CaseInsensitive)
+        if checkReg.firstMatchInString(getValue, options: .ReportCompletion, range: NSMakeRange(0, getValue.characters.count)) != nil {
+            checkReg.pattern
+            print("The streak is broken!")
+        }
+        else{
+            print("The streak lives still in our heart!")
+        }
+    }
+    catch{
+        print("Error")
+    }
+    noCases -= 1
+}
